a3e1d855748ead57f7a87bb2e9ecc8a6efb2d7df
Jonathan Cruz <cruzjonathan.e@gmail.com>
2023-12-12 18:17:37 -0300

    Clase 01 - Variables

---
+++ Clase_01/clase01.js
@@ -0,0 +1,30 @@
+// String -> texto -> cadena
+const DNI = 1234;
+let usuario1 = 'John';
+let usuario2 = 'Micaela';
+
+
+let nombre = "Jonathan ";
+let apellido = 'Cruz';
+
+// Numbre -> numero
+let edad = 38;  // Entero
+let altura = 178.5 // Decimal -> float
+//let año  No se pueden usar ñ ó
+nombre = 'Lucas';
+nombre = 'Matias';
+// 🛑 No se puede reasignar un valor a una constante
+// DNI = 2244;
+
+// 🟦 OPERACIONES 
+let numero1 = 3;
+let numero2 = 2;
+let color = 'azul';
+let nombreApellido = nombre + ' ' + apellido;
+
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
---
+++ Clase_01/funciones.js
@@ -0,0 +1,8 @@
+let usuario = prompt('Ingrese el usuario');
+let rol = prompt('Ingrese el rol ');
+// let edad = prompt('Ingrese la edad');
+let mensaje = 'Hola ' + rol + ': ' + usuario;
+console.log(mensaje);
+
+alert( mensaje);
+
