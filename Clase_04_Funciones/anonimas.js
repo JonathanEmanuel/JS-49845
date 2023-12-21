40df3e7bf7c9841cb322833b0aa444208a45ca2b
Jonathan Cruz <cruzjonathan.e@gmail.com>
2023-12-21 19:24:28 -0300

    Clase 04 - Funciones anonimas

---
+++ Clase_04_Funciones/anonimas.js
@@ -0,0 +1,14 @@
+const nombre = 'Juan';
+const dni = 1234;
+const casado = true;
+
+const saludar = function(nombre){
+    alert('Hola ' + nombre + '. Soy una función');
+}
+
+// Función Fecha
+const mostrarDia = (dia, mes) => alert('Hoy es ' + dia + ' Mes ' + mes);
+
+mostrarDia('Jueves', 'Diciembre');
+
+
