bc432f2cc609edf75f41856b1c93e13a1ac7c2d0
Jonathan Cruz <cruzjonathan.e@gmail.com>
2023-12-22 20:38:17 -0300

    Clase 05 - Objetos

--- Clase_05_objetos/objetos.js
+++ Clase_05_objetos/objetos.js
@@ -8,14 +8,28 @@ const saludar = function(nombre){  // function
 
 // Defino un objeto literal
 const persona = { 
-    nombre: 'Lucas',        // Los : se utilizan SOLO en los Objeto 
+    // ATRIBUTOS
+    nombre: 'Rocio',        // Los : se utilizan SOLO en los Objeto 
     apellido: 'Ruiz', 
     dni: 2342,
-    casado: false
+    casado: false,
+    edad: 21,
+    // MÉTODOS
+    saludar: function(){
+        alert('Hola soy ' + this.nombre);
+    },
+    actualizarEstado: function(estado){
+        this.casado = estado;
+    },
+    cumple: function(){
+        this.edad ++;
+        //this.edad = this.edad + 1;
+    }
 }
 
 persona.tel = '11-2234-2222';
 
+persona.saludar();
 
 /* console.log( persona );
 console.log(  persona.nombre  );
@@ -24,7 +38,7 @@ console.log(  typeof( persona )  ); */
 console.log( 'Esta casado ' + persona.casado );
 // Actualizo un valor del objeto 
 persona.casado = true;
-persona['nombre'] = 'Lucas Segundo';
+//persona['nombre'] = 'Lucas Segundo';
 
 
 console.log( 'Esta casado ' + persona.casado );
@@ -35,8 +49,15 @@ console.log('Ahora me llamo ' + persona['nombre'] );
 
 // Objetos con función constructora 
 function Producto( nombre, precio ){
+    // Atributos
     this.nombre = nombre;
     this.precio = precio;
+    this.precioFINAL = 0;
+    // Métodos
+    this.sumarIVA = function(){
+        this.precioFINAL = this.precio * 1.21;
+        alert('El precio es $' + this.precio + ' | Con IVA es $' + this.precioFINAL)
+    }
 }
 
 const producto1 = new Producto('Tv 32', 36000);
@@ -44,7 +65,26 @@ const producto2 = new Producto('Laptop Lenovo', 600000);
 const producto3 = new Producto('Cafetera', 12000);
 const producto4 = new Producto('Microfono', 6000);
 
+producto1.sumarIVA();
 
 console.log( producto1);
 console.log( producto1.nombre );
 
+// VERSION CON CLASES
+class Perro {
+    constructor(nombre, color){
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    ladrar(){
+        alert('woof');
+    }
+}
+
+const perro1 = new Perro('Laika', 'Negro');
+const perro2 = new Perro('Kiyomi', 'Blanco');
+const perro3 = new Perro('pancho', 'Marron');
+
+
+console.log(perro1);
