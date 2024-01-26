a9f5119bb109de6fce61183f18ed369fd13b2483
Jonathan Cruz <cruzjonathan.e@gmail.com>
2024-01-26 20:39:27 -0300

    AFTER IV

---
+++ Afters/After_04/data.js
@@ -0,0 +1,7 @@
+const products = [
+    { id: 1, name: 'WHEELJACK GC606', img: 'images/gabinete.png', price: 1000, category: 'computacion' },
+    { id: 2, name: 'SAM SCYLLA', img: 'images/audio.png', price: 200, category: 'computacion' },
+    { id: 3, name: 'DRAGONBORN K630', img: 'images/teclado.png', price: 300, category: 'computacion' },
+    { id: 4, name: 'SAMSUNG A13', img: 'images/gabinete.png', price: 500, category: 'celular' }
+]
+
