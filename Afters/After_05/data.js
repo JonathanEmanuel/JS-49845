2f14c7ff5bb98cbfbf01806a0c12769887f7c0c4
Jonathan Cruz <cruzjonathan.e@gmail.com>
2024-02-07 19:16:38 -0300

    After 05

---
+++ Afters/After_05/data.js
@@ -0,0 +1,7 @@
+const products = [
+    { id: 1, name: 'WHEELJACK GC606', img: 'images/gabinete.png', price: 1000, category: 'computacion' },
+    { id: 2, name: 'Auriculares Motorola', img: 'images/audio.png', price: 200, category: 'computacion' },
+    { id: 3, name: 'Teclado DRAGONBORN K630', img: 'images/teclado.png', price: 300, category: 'computacion' },
+    { id: 4, name: 'Celular Motorola E22', img: 'images/motorola.jpg', price: 500, category: 'celular' }
+]
+
